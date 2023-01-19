5113d5b2845ba859b28a11f1ffc8b791a5a25e10
mago mahi <maheshhonnamurthy099@gmail.com>
2023-01-19 15:56:28 +0530

    [Mahesh]Add:Check Employee daily part or full time wage

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -17,3 +17,29 @@ else
 }
 let myattendence =new Attendence();
 myattendence.attendence();
+
+
+//Calculate Daily Employee Wage based on part time or full time work
+
+   
+        const Part_Time=1;
+const Full_Time=2;
+const Part_Time_Hours=4;
+const Full_Time_Hours=8;
+const Wage_per_Hours=20;
+
+let empHrs=0;
+empcheck=Math.floor(Math.random()*10)%3;
+switch(empcheck)
+{
+    case Part_Time:
+        empHrs=Part_Time_Hours;
+        break;
+     case Full_Time:
+        empHrs=Full_Time_Hours;
+        break;
+    default:
+        empHrs=0;
+}
+let empWage=empHrs*Wage_per_Hours;
+console.log("Emp Wage: " +empWage);
